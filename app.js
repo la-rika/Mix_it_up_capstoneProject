15256d5f309d80f3a1410d2f3cb0ae354ce32f4c
la-rika <laramisiaforlani@gmail.com>
2023-11-26 23:01:39 +0100

    started homepage layout

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import ejs from 'ejs';
+import { getDirName } from './utils.js'
+
+const dirName = getDirName(import.meta.url)
 
 const app = express();
 const port = 3000;
+app.use(express.static(dirName + '/public'));
 
 app.get('/',(req,res)=>{
     res.render('homepage.ejs')
---
+++ utils.js
@@ -0,0 +1,12 @@
+
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const getDirName = function (moduleUrl) {
+    const filename = fileURLToPath(moduleUrl)
+    return path.dirname(filename)
+}
+
+export {
+    getDirName
+}
